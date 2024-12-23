e57102832234770e71ed0caf9f5bac04ee650ede
Eduardo Carlos Santos Pereira Junior <eduardo_carlos@grupotiradentes.com>
2024-12-23 20:18:21 -0300

    Endpoints e status code

---
+++ src/server/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const router = Router();
+
+router.get('/home', (_, res) => {
+    return res.send('Olá DEV!');
+});
+
+router.post('/teste', (req, res) => {
+    console.log(req);
+
+    // return res.send('Teste de atualização');
+    return res.status(StatusCodes.UNAUTHORIZED).josn(req.body);
+});
+
+export { router }
